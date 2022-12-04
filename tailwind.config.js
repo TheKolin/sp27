3bdc302799096b20b81c09b57a56aa8f48c1b156
Marcin Lukasiewicz <marcin@tralala-group.com>
2022-12-04 23:44:56 +0100

    new layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: [
-        "./assets/**/*.{vue,js,ts,jsx,tsx}",
-        "./templates/**/*.{html,twig}",
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
     ],
     theme: {
-      extend: {
-          fontFamily: {
-              sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-          },
-      }
+        extend: {
+            colors: {
+                'logi-blue': '#04A5E5',
+                'logi-blue-dark': '#026d97',
+                'logi-dark': '#211F21',
+                'logi-white': '#F4F3F4',
+                'logi-black': '#080608',
+                'sbs-green-dark': '#38661d',
+                'sbs-green': '#66B933'
+            }
+        },
     },
     plugins: [],
 }
