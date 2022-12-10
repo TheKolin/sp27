e34f1ea11b9f4790c302f9449f94ec16eff78967
Marcin Lukasiewicz <marcin@tralala-group.com>
2022-12-10 15:30:16 +0100

    new layout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,42 +5,62 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
-{
+class SecurityController extends AbstractController {
+
     /**
      * @Route(
-     *      path="/",
-     *      name="login",
+     *     path="/",
+     *     methods={"GET", "POST"},
+     *     name="login"
      * )
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function login(AuthenticationUtils $authenticationUtils): Response {
         if ($this->getUser()) {
             return $this->redirectToRoute('dashboard');
         }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig');
+    }
 
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername, 
-            'error' => $error
+    /**
+     * @Route(
+     *     path="/ajax/login",
+     *     methods={"GET", "POST"},
+     *     name="ajax.login",
+     * )
+     */
+    public function loginJson(AuthenticationUtils $authenticationUtils): Response {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        if ($error) {
+            return $this->json([
+                'error' => $error->getMessageKey()
+            ]);
+//        } else if (!$this->isGranted(AuthenticatedVoter::IS_AUTHENTICATED_FULLY)) {
+//            return $this->json([
+//                'error' => 'Invalid request type'
+//            ]);
+        }
+
+        $user = $this->getUser();
+
+        return $this->json([
+            'roles' => $user->getRoles(),
+            'redirect' => $this->generateUrl('dashboard')
         ]);
     }
 
     /**
      * @Route(
-     *      path="/logout",
-     *      methods={"GET"},
-     *      name="logout",
+     *     path="/logout",
+     *     methods={"GET"},
+     *     name="logout",
      * )
      */
-    public function logout(): void
-    {
+    public function logout(): void {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
